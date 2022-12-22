5374e32350ef2c3b3eb704cf9a4c4a114a14ea71
Soniabensaad <5687@holbertonstudents.com>
2022-12-22 22:54:31 +0100

    monty prog

--- monty.h
+++ monty.h
@@ -49,6 +49,7 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+#endif
 
 
 
